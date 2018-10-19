6f1fd1fc40d69d54e2a8ed050f93ca7a234e8b23
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-10-19 14:41:51 -0500

    Implement home screen template.

--- src/main/java/com/example/cms/poc/directus/DirectusApplication.java
+++ src/main/java/com/example/cms/poc/directus/DirectusApplication.java
@@ -2,11 +2,12 @@ package com.example.cms.poc.directus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DirectusApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DirectusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DirectusApplication.class, args);
+    }
 }
