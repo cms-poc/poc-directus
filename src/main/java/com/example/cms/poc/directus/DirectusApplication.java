ef13a489c9493fd606731ded7626e7bbdb09e18a
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-10-19 14:35:25 -0500

    Generate project from Spring Initializr.

---
+++ src/main/java/com/example/cms/poc/directus/DirectusApplication.java
@@ -0,0 +1,12 @@
+package com.example.cms.poc.directus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectusApplication.class, args);
+	}
+}
